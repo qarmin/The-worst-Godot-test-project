bfc31ff72d9b94c1574f3b8367deeef6cbafb2e3
qarmin <mikrutrafal54@gmail.com>
2019-06-06 12:25:05 +0200

    Bug with get_word_under_cursor

--- ControlALL/TextEdit.gd
+++ ControlALL/TextEdit.gd
@@ -21,6 +21,8 @@ func _process(delta) -> void:
 #		qq = qq
 	
 	if counter <= 0:
+		var qq : String = ""
+		
 		set_text("asfasf\nafq" + str(randf() * 50) + "sgerafsaqw")
 		set_readonly(bool(randi()%2))
 		set_highlight_current_line(bool(randi()%2))
@@ -44,48 +46,48 @@ func _process(delta) -> void:
 		
 		add_color_region("a", "qw", Color(randf(),randf(),randf(),randf()),bool(randi()%2))
 		add_keyword_color("sfafq", Color(randf(),randf(),randf(),randf()))
-		can_fold(randi() % get_line_count())
+		qq += str(can_fold(randi() % get_line_count()))
 		clear_colors()
 		clear_undo_history()
 		copy()
-		cursor_get_column()
-		cursor_get_line()
+		qq += str(cursor_get_column())
+		qq += str(cursor_get_line())
 		cursor_set_column(cursor_get_column())
 		cursor_set_line(cursor_get_line())
 		cut()
 		deselect()
 		fold_all_lines()
 		fold_line(randi() % get_line_count())
-		get_breakpoints()
-		get_keyword_color("sfafq")
-		get_line(randi() % get_line_count())
-		get_line_count()
-		get_menu()
-		get_selection_from_column()
-		get_selection_from_line()
-		get_selection_text()
-		get_selection_to_column()
-		get_selection_to_line()
-		get_word_under_cursor()
-		has_keyword_color("sfafq")
+		qq += str(get_breakpoints())
+		#qq += str(get_keyword_color("sfafq"))
+		qq += str(get_line(randi() % get_line_count()))
+		qq += str(get_line_count())
+		qq += str(get_menu())
+		#qq += str(get_selection_from_column())
+		#qq += str(get_selection_from_line())
+		#qq += str(get_selection_text())
+		#qq += str(get_selection_to_column())
+		#qq += str(get_selection_to_line())
+		qq += str(get_word_under_cursor())
+		qq += str(has_keyword_color("sfafq"))
 		insert_text_at_cursor(str(randf()))
-		is_folded(randi() % get_line_count())
-		is_line_hidden(randi() % get_line_count())
-		is_selection_active()
+		qq += str(is_folded(randi() % get_line_count()))
+		qq += str(is_line_hidden(randi() % get_line_count()))
+		qq += str(is_selection_active())
 		menu_option(randi() % MENU_MAX)
 		paste()
 		redo()
 		remove_breakpoints()
-		search("asf", randi() % 8,0,0) # Strange but corrent, Match case, whole words and backwards can be added to itself
+		#qq += str(search("asf", randi() % 8,0,0)) # Strange but corrent, Match case, whole words and backwards can be added to itself
 		select(0,0,get_line_count(),1)
 		select_all()
-		set_line_as_hidden(get_line_count(),bool(randi()%2))
-		toggle_fold_line(get_line_count())
+		#set_line_as_hidden(get_line_count(),bool(randi()%2))
+		#toggle_fold_line(get_line_count())
 		undo()
-		unfold_line(get_line_count())
+		#unfold_line(get_line_count())
 		unhide_all_lines()
 		
-		
+		qq = qq
 		
 		
 		
