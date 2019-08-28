3c59af5cda28ad13745328a49aa4ec66001b00af
qarmin <mikrutrafal54@gmail.com>
2019-08-28 17:36:03 +0200

    Basics

--- 3DALL/3DALL.gd
+++ 3DALL/3DALL.gd
@@ -2,14 +2,20 @@ extends Spatial
 
 const TIME_TO_ACTIVATE : Vector2 = Vector2(0.1,0.2)
 var counter_to_delete : int = 10000000000
+const TIME_TO_NEXT_NODE : float = 1.0
+var current_time_to_next_node : float = TIME_TO_NEXT_NODE
+var current_node : int = 0
 
 func _ready() -> void:
 	if !is_visible():
 		queue_free()
+		
 	if Autoload.BASIC_COUTER > 0:
 		counter_to_delete = Autoload.BASIC_COUTER
 	for i in get_children():
 		i.C_COUNTER = TIME_TO_ACTIVATE
+		if Autoload.USE_ONLY_ONE_NODE:
+			i.set_process(false)
 		if !i.is_visible():
 			i.queue_free()
 
@@ -19,3 +25,14 @@ func _process(_delta: float) -> void:
 		for i in get_children():
 			i.queue_free()
 		get_tree().quit()
+	
+	if Autoload.USE_ONLY_ONE_NODE && current_time_to_next_node <= 0.0:
+		current_time_to_next_node = TIME_TO_NEXT_NODE
+		if get_child_count() > current_node:
+			get_child(current_node).set_process(false)
+			Autoload.save_to_file("Stop: " + get_child(current_node).get_name())
+		current_node = (current_node + 1) % get_child_count()
+		if get_child_count() > current_node:
+			get_child(current_node).set_process(true)
+			Autoload.save_to_file("Start: " + get_child(current_node).get_name())
+			
--- Autoload.gd
+++ Autoload.gd
@@ -3,7 +3,21 @@ extends Node
 const WRONG_BUGS : bool = true
 const BASIC_COUTER : int = -10# smaller than 0 doesn't exit project
 const SLOW_FUNCTIONS : bool = false # execute slow functions, for performance reasons should be used rarely
+const USE_ONLY_ONE_NODE : bool = true
+
+var file : File = File.new()
+
+func _ready():
+	if file.open("usr://dane.txt",File.WRITE) != OK:
+		printerr("AAAAAAAAAAA")
+		get_tree().quit()
+
+
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().quit()
+
+func save_to_file(message : String) -> void:
+	file.store_line(message)
+	
