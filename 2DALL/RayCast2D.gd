2e3ec9cec7ebb344055b3c86c264ffb756c3a24b
qarmin <mikrutrafal54@gmail.com>
2019-10-23 13:02:54 +0200

    Prevent too big RayCast perfromance hit

--- 2DALL/RayCast2D.gd
+++ 2DALL/RayCast2D.gd
@@ -17,42 +17,43 @@ func nodeFunction(q_RayCast2D : RayCast2D, can_reset : bool = false) -> void:
 		AutoObjects.A_Node(q_RayCast2D)
 		AutoObjects.A_CanvasItem(q_RayCast2D)
 		AutoObjects.A_Node2D(q_RayCast2D)
-		
-	if randi() % 2 == 1:
-		q_RayCast2D.clear_exceptions()
-	if randi() % 2 == 1:
-		q_RayCast2D.set_enabled(Autoload.get_bool())
-	if randi() % 2 == 1:
-		q_RayCast2D.set_exclude_parent_body(Autoload.get_bool())
-	if randi() % 2 == 1:
-		q_RayCast2D.set_cast_to(Autoload.get_vector2())
-	if randi() % 2 == 1:
-		q_RayCast2D.set_collision_mask(Autoload.get_int())
-	if randi() % 2 == 1:
-		q_RayCast2D.set_collide_with_areas(Autoload.get_bool())
-	if randi() % 2 == 1:
-		q_RayCast2D.set_collide_with_bodies(Autoload.get_bool())
-	if randi() % 2 == 1:
-		q_RayCast2D.add_exception(get_tree().get_root())
-	if randi() % 2 == 1:
-		q_RayCast2D.add_exception_rid(RID())
-	if randi() % 2 == 1:
-		q_RayCast2D.force_raycast_update()
-	if randi() % 2 == 1:
-		q_RayCast2D.get_collider()
-	if randi() % 2 == 1:
-		q_RayCast2D.get_collider_shape()
-	if randi() % 2 == 1:
-		q_RayCast2D.get_collision_mask_bit(Autoload.get_int())
-	if randi() % 2 == 1:
-		q_RayCast2D.get_collision_normal()
-	if randi() % 2 == 1:
-		q_RayCast2D.get_collision_point()
-	if randi() % 2 == 1:
-		q_RayCast2D.is_colliding()
-	if randi() % 2 == 1:
-		q_RayCast2D.remove_exception(get_tree().get_root())
-	if randi() % 2 == 1:
-		q_RayCast2D.remove_exception_rid(RID())
-	if randi() % 2 == 1:
-		q_RayCast2D.set_collision_mask_bit(Autoload.get_int(),Autoload.get_bool())
+# SLOW
+#	if randi() % 2 == 1:
+#		q_RayCast2D.clear_exceptions()
+#	if randi() % 2 == 1:
+#		q_RayCast2D.set_enabled(Autoload.get_bool())
+#	if randi() % 2 == 1:
+#		q_RayCast2D.set_exclude_parent_body(Autoload.get_bool())
+#	if randi() % 2 == 1:
+#		q_RayCast2D.set_cast_to(Autoload.get_vector2f(100.0))
+#	if randi() % 2 == 1:
+#		q_RayCast2D.set_collision_mask(Autoload.get_int())
+#	if randi() % 2 == 1:
+#		q_RayCast2D.set_collide_with_areas(Autoload.get_bool())
+#	if randi() % 2 == 1:
+#		q_RayCast2D.set_collide_with_bodies(Autoload.get_bool())
+#	if randi() % 2 == 1:
+#		q_RayCast2D.add_exception(get_tree().get_root())
+#	if randi() % 2 == 1:
+#		q_RayCast2D.add_exception_rid(RID())
+#	if Autoload.SLOW_FUNCTIONS:
+#		if randi() % 2 == 1:
+#			q_RayCast2D.force_raycast_update()
+#	if randi() % 2 == 1:
+#		q_RayCast2D.get_collider()
+#	if randi() % 2 == 1:
+#		q_RayCast2D.get_collider_shape()
+#	if randi() % 2 == 1:
+#		q_RayCast2D.get_collision_mask_bit(Autoload.get_int())
+#	if randi() % 2 == 1:
+#		q_RayCast2D.get_collision_normal()
+#	if randi() % 2 == 1:
+#		q_RayCast2D.get_collision_point()
+#	if randi() % 2 == 1:
+#		q_RayCast2D.is_colliding()
+#	if randi() % 2 == 1:
+#		q_RayCast2D.remove_exception(get_tree().get_root())
+#	if randi() % 2 == 1:
+#		q_RayCast2D.remove_exception_rid(RID())
+#	if randi() % 2 == 1:
+#		q_RayCast2D.set_collision_mask_bit(Autoload.get_int(),Autoload.get_bool())
