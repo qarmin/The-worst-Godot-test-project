4c6579ac0781d2d9bd4526a82e3ac7e4a79ecf52
Rafał Mikrut <mikrutrafal54@gmail.com>
2020-08-03 16:08:56 +0200

    Add timeout

--- Autoloads/Autoload.gd
+++ Autoloads/Autoload.gd
@@ -5,14 +5,27 @@ const NOT_A_BUG : bool = false #execute functions which doesn't contains bugs, b
 
 const RANGE : int = 1
 
-var frames_to_show = -1
+var frames_to_show : int = -1
+var time_to_show : float = -1000.0
 
 ############### FUNCTIONS
+func _init() -> void:
+	for argument in OS.get_cmdline_args():
+		var rr : String = argument
+		if rr.find("tscn") != -1: # Ignore all tscn scenes/names
+			continue
+		time_to_show = argument.to_float()
+		print("Time set to: " + str(time_to_show))
+		break
 
-func _process(delta):
+func _process(delta : float) -> void:
+	time_to_show -= delta
+	if time_to_show < 0 && time_to_show > -500:
+		get_tree().quit()
+	
+	frames_to_show -= 1
 	if frames_to_show == 0:
 		get_tree().quit()
-	frames_to_show -= 1
 
 
 func get_rand_time():
