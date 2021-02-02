ee6f4e1743b6bad0c2fe8e4115cb967f06a41b9f
Rafał Mikrut <mikrutrafal@protonmail.com>
2021-02-02 10:33:29 +0100

    String text

--- ResourcesALL/String.gd
+++ ResourcesALL/String.gd
@@ -20,174 +20,174 @@ func nodeFunction(q_String: String, can_reset: bool = false) -> void:
 			if randi() % 2 == 1:
 				q_String = String(Autoload.get_nodepath(self))
 
-	if randi() % 2 == 1:
-		q_String.begins_with(Autoload.get_string())
-	if randi() % 2 == 1:
-		q_String.bigrams()
-	if randi() % 2 == 1:
-		q_String.c_escape()
-	if randi() % 2 == 1:
-		q_String.c_unescape()
-	if randi() % 2 == 1:
-		q_String.capitalize()
-	if randi() % 2 == 1:
-		q_String.casecmp_to(Autoload.get_string())
-	if randi() % 2 == 1:
-		q_String.dedent()
-	if randi() % 2 == 1:
-		q_String.is_empty()
-	if randi() % 2 == 1:
-		q_String.ends_with(Autoload.get_string())
 #	if randi() % 2 == 1:
-#		q_String.erase(Autoload.get_int(), Autoload.get_int())
-
-	if randi() % 2 == 1:
-		q_String.find(Autoload.get_string(), Autoload.get_int())
-	if randi() % 2 == 1:
-		q_String.findn(Autoload.get_string(), Autoload.get_int())
-
-	if randi() % 2 == 1:
-		q_String.format(Autoload.get_string(), Autoload.get_string())
-
-	if randi() % 2 == 1:
-		q_String.get_base_dir()
-	if randi() % 2 == 1:
-		q_String.get_basename()
-	if randi() % 2 == 1:
-		q_String.get_extension()
-	if randi() % 2 == 1:
-		q_String.get_file()
-
-	if randi() % 2 == 1:
-		q_String.hash()
-	if randi() % 2 == 1:
-		q_String.hex_to_int()
-
-	if randi() % 2 == 1:
-		q_String.http_escape()
-	if randi() % 2 == 1:
-		q_String.http_unescape()
-
-	if randi() % 2 == 1:
-		q_String.insert(Autoload.get_int(), Autoload.get_string())
-
-	if randi() % 2 == 1:
-		q_String.is_abs_path()
-	if randi() % 2 == 1:
-		q_String.is_rel_path()
-	if randi() % 2 == 1:
-		q_String.is_subsequence_of(Autoload.get_string())
-	if randi() % 2 == 1:
-		q_String.is_subsequence_ofi(Autoload.get_string())
-	if randi() % 2 == 1:
-		q_String.is_valid_filename()
-	if randi() % 2 == 1:
-		q_String.is_valid_float()
-	if randi() % 2 == 1:
-		q_String.is_valid_hex_number(Autoload.get_bool())
-	if randi() % 2 == 1:
-		q_String.is_valid_html_color()
-	if randi() % 2 == 1:
-		q_String.is_valid_identifier()
-	if randi() % 2 == 1:
-		q_String.is_valid_integer()
-	if randi() % 2 == 1:
-		q_String.is_valid_ip_address()
-
-	if randi() % 2 == 1:
-		q_String.json_escape()
-	if randi() % 2 == 1:
-		q_String.left(Autoload.get_int())
-	if randi() % 2 == 1:
-		q_String.length()
-	if randi() % 2 == 1:
-		q_String.lstrip(Autoload.get_string())
-
-#	if randi() % 2 == 1: #GH#40740
-#		q_String.match(Autoload.get_string())
-	if randi() % 2 == 1:
-		q_String.matchn(Autoload.get_string())
-
-	if randi() % 2 == 1:
-		q_String.md5_buffer()
-	if randi() % 2 == 1:
-		q_String.md5_text()
-
-	if randi() % 2 == 1:
-		q_String.nocasecmp_to(Autoload.get_string())
-	if randi() % 2 == 1:
-		q_String.ord_at(Autoload.get_int())
-
-	if randi() % 2 == 1:
-		q_String.pad_decimals(Autoload.get_inti(100))
-	if randi() % 2 == 1:
-		q_String.pad_zeros(Autoload.get_inti(100))
-
-	if randi() % 2 == 1:
-		q_String.percent_decode()
-	if randi() % 2 == 1:
-		q_String.percent_encode()
-
-	if randi() % 2 == 1:
-		q_String.plus_file(Autoload.get_string())
-
-	if randi() % 2 == 1:
-		q_String.replace(Autoload.get_string(), Autoload.get_string())
-	if randi() % 2 == 1:
-		q_String.replacen(Autoload.get_string(), Autoload.get_string())
-
-	if randi() % 2 == 1:
-		q_String.rfind(Autoload.get_string(), Autoload.get_int())
-	if randi() % 2 == 1:
-		q_String.rfindn(Autoload.get_string(), Autoload.get_int())
-
-	if randi() % 2 == 1:
-		q_String.right(Autoload.get_int())
-	if randi() % 2 == 1:
-		q_String.rsplit(Autoload.get_string(), Autoload.get_bool(), Autoload.get_int())
-	if randi() % 2 == 1:
-		q_String.rstrip(Autoload.get_string())
-
-	if randi() % 2 == 1:
-		q_String.sha256_buffer()
-	if randi() % 2 == 1:
-		q_String.sha256_text()
-
-	if randi() % 2 == 1:
-		q_String.similarity(Autoload.get_string())
-
-	if randi() % 2 == 1:
-		q_String.split(Autoload.get_string(), Autoload.get_bool(), Autoload.get_int())
-	if randi() % 2 == 1:
-		q_String.split_floats(Autoload.get_string(), Autoload.get_bool())
-
-	if randi() % 2 == 1:
-		q_String.strip_edges(Autoload.get_bool(), Autoload.get_bool())
-	if randi() % 2 == 1:
-		q_String.strip_escapes()
-
-	if randi() % 2 == 1:
-		q_String.substr(Autoload.get_int(), Autoload.get_int())
-
+#		q_String.begins_with(Autoload.get_string())
 #	if randi() % 2 == 1:
-#		q_String.to_ascii()
-	if randi() % 2 == 1:
-		q_String.to_float()
-	if randi() % 2 == 1:
-		q_String.to_int()
-	if randi() % 2 == 1:
-		q_String.to_lower()
-	if randi() % 2 == 1:
-		q_String.to_upper()
+#		q_String.bigrams()
 #	if randi() % 2 == 1:
-#		q_String.to_utf8()
-
-	if randi() % 2 == 1:
-		q_String.trim_prefix(Autoload.get_string())
-	if randi() % 2 == 1:
-		q_String.trim_suffix(Autoload.get_string())
-
-	if randi() % 2 == 1:
-		q_String.xml_escape()
-	if randi() % 2 == 1:
-		q_String.xml_unescape()
+#		q_String.c_escape()
+#	if randi() % 2 == 1:
+#		q_String.c_unescape()
+#	if randi() % 2 == 1:
+#		q_String.capitalize()
+#	if randi() % 2 == 1:
+#		q_String.casecmp_to(Autoload.get_string())
+#	if randi() % 2 == 1:
+#		q_String.dedent()
+#	if randi() % 2 == 1:
+#		q_String.is_empty()
+#	if randi() % 2 == 1:
+#		q_String.ends_with(Autoload.get_string())
+##	if randi() % 2 == 1:
+##		q_String.erase(Autoload.get_int(), Autoload.get_int())
+#
+#	if randi() % 2 == 1:
+#		q_String.find(Autoload.get_string(), Autoload.get_int())
+#	if randi() % 2 == 1:
+#		q_String.findn(Autoload.get_string(), Autoload.get_int())
+#
+#	if randi() % 2 == 1:
+#		q_String.format(Autoload.get_string(), Autoload.get_string())
+#
+#	if randi() % 2 == 1:
+#		q_String.get_base_dir()
+#	if randi() % 2 == 1:
+#		q_String.get_basename()
+#	if randi() % 2 == 1:
+#		q_String.get_extension()
+#	if randi() % 2 == 1:
+#		q_String.get_file()
+#
+#	if randi() % 2 == 1:
+#		q_String.hash()
+#	if randi() % 2 == 1:
+#		q_String.hex_to_int()
+#
+#	if randi() % 2 == 1:
+#		q_String.http_escape()
+#	if randi() % 2 == 1:
+#		q_String.http_unescape()
+#
+#	if randi() % 2 == 1:
+#		q_String.insert(Autoload.get_int(), Autoload.get_string())
+#
+#	if randi() % 2 == 1:
+#		q_String.is_abs_path()
+#	if randi() % 2 == 1:
+#		q_String.is_rel_path()
+#	if randi() % 2 == 1:
+#		q_String.is_subsequence_of(Autoload.get_string())
+#	if randi() % 2 == 1:
+#		q_String.is_subsequence_ofi(Autoload.get_string())
+#	if randi() % 2 == 1:
+#		q_String.is_valid_filename()
+#	if randi() % 2 == 1:
+#		q_String.is_valid_float()
+#	if randi() % 2 == 1:
+#		q_String.is_valid_hex_number(Autoload.get_bool())
+#	if randi() % 2 == 1:
+#		q_String.is_valid_html_color()
+#	if randi() % 2 == 1:
+#		q_String.is_valid_identifier()
+#	if randi() % 2 == 1:
+#		q_String.is_valid_integer()
+#	if randi() % 2 == 1:
+#		q_String.is_valid_ip_address()
+#
+#	if randi() % 2 == 1:
+#		q_String.json_escape()
+#	if randi() % 2 == 1:
+#		q_String.left(Autoload.get_int())
+#	if randi() % 2 == 1:
+#		q_String.length()
+#	if randi() % 2 == 1:
+#		q_String.lstrip(Autoload.get_string())
+#
+##	if randi() % 2 == 1: #GH#40740
+##		q_String.match(Autoload.get_string())
+#	if randi() % 2 == 1:
+#		q_String.matchn(Autoload.get_string())
+#
+#	if randi() % 2 == 1:
+#		q_String.md5_buffer()
+#	if randi() % 2 == 1:
+#		q_String.md5_text()
+#
+#	if randi() % 2 == 1:
+#		q_String.nocasecmp_to(Autoload.get_string())
+#	if randi() % 2 == 1:
+#		q_String.ord_at(Autoload.get_int())
+#
+#	if randi() % 2 == 1:
+#		q_String.pad_decimals(Autoload.get_inti(100))
+#	if randi() % 2 == 1:
+#		q_String.pad_zeros(Autoload.get_inti(100))
+#
+#	if randi() % 2 == 1:
+#		q_String.percent_decode()
+#	if randi() % 2 == 1:
+#		q_String.percent_encode()
+#
+#	if randi() % 2 == 1:
+#		q_String.plus_file(Autoload.get_string())
+#
+#	if randi() % 2 == 1:
+#		q_String.replace(Autoload.get_string(), Autoload.get_string())
+#	if randi() % 2 == 1:
+#		q_String.replacen(Autoload.get_string(), Autoload.get_string())
+#
+#	if randi() % 2 == 1:
+#		q_String.rfind(Autoload.get_string(), Autoload.get_int())
+#	if randi() % 2 == 1:
+#		q_String.rfindn(Autoload.get_string(), Autoload.get_int())
+#
+#	if randi() % 2 == 1:
+#		q_String.right(Autoload.get_int())
+#	if randi() % 2 == 1:
+#		q_String.rsplit(Autoload.get_string(), Autoload.get_bool(), Autoload.get_int())
+#	if randi() % 2 == 1:
+#		q_String.rstrip(Autoload.get_string())
+#
+#	if randi() % 2 == 1:
+#		q_String.sha256_buffer()
+#	if randi() % 2 == 1:
+#		q_String.sha256_text()
+#
+#	if randi() % 2 == 1:
+#		q_String.similarity(Autoload.get_string())
+#
+#	if randi() % 2 == 1:
+#		q_String.split(Autoload.get_string(), Autoload.get_bool(), Autoload.get_int())
+#	if randi() % 2 == 1:
+#		q_String.split_floats(Autoload.get_string(), Autoload.get_bool())
+#
+#	if randi() % 2 == 1:
+#		q_String.strip_edges(Autoload.get_bool(), Autoload.get_bool())
+#	if randi() % 2 == 1:
+#		q_String.strip_escapes()
+#
+#	if randi() % 2 == 1:
+#		q_String.substr(Autoload.get_int(), Autoload.get_int())
+#
+##	if randi() % 2 == 1:
+##		q_String.to_ascii()
+#	if randi() % 2 == 1:
+#		q_String.to_float()
+#	if randi() % 2 == 1:
+#		q_String.to_int()
+#	if randi() % 2 == 1:
+#		q_String.to_lower()
+#	if randi() % 2 == 1:
+#		q_String.to_upper()
+##	if randi() % 2 == 1:
+##		q_String.to_utf8()
+#
+#	if randi() % 2 == 1:
+#		q_String.trim_prefix(Autoload.get_string())
+#	if randi() % 2 == 1:
+#		q_String.trim_suffix(Autoload.get_string())
+#
+#	if randi() % 2 == 1:
+#		q_String.xml_escape()
+#	if randi() % 2 == 1:
+#		q_String.xml_unescape()
